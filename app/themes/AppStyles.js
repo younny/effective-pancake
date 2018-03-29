b7bc61a0ca792c44be3ee7d45f717b0d1aa74077
Gi Dooyoung <dooyoung@favoritemedium.com>
2018-03-29 14:26:53 +0900

    globl metrics added

--- app/themes/AppStyles.js
+++ app/themes/AppStyles.js
@@ -1,11 +1,13 @@
 import Fonts from './Fonts'
 import Colors from './Colors'
+import Metrics from './Metrics'
 
 /* This file contains common styles from screens and components. */
 export default {
     screen: {
         container: {
-            flex: 1
+            flex: 1,
+            padding: Metrics.pd.base
         }
     },
     component: {
--- app/themes/Metrics.js
+++ app/themes/Metrics.js
@@ -3,6 +3,10 @@ import { Dimensions, Platform, PixelRatio } from 'react-native'
 const { width, height } = Dimensions.get('window')
 
 export default {
-    screenWidth: width,
-    screenHeight: height
+    screenWidth: width, //TODO. Handle device rotation.
+    screenHeight: height,
+    pd: {
+        base: 10,
+        double: 20
+    }
 }
